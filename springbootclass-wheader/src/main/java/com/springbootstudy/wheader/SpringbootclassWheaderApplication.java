186a3f45b330e952613170e31f4cdb1e1997d9f3
jangmi <jsy639@naver.com>
2024-11-19 17:27:52 +0900

    연습용 날씨 사이트 첫 커밋

---
+++ springbootclass-wheader/src/main/java/com/springbootstudy/wheader/SpringbootclassWheaderApplication.java
@@ -0,0 +1,13 @@
+package com.springbootstudy.wheader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootclassWheaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootclassWheaderApplication.class, args);
+	}
+
+}
