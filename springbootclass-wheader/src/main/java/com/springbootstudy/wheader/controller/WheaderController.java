dcde1f6d9319d815fc84ec100df01d6be3e74463
jangmi <jsy639@naver.com>
2024-11-20 16:33:43 +0900

    새로 템플릿 연결함

--- springbootclass-wheader/src/main/java/com/springbootstudy/wheader/controller/WheaderController.java
+++ springbootclass-wheader/src/main/java/com/springbootstudy/wheader/controller/WheaderController.java
@@ -10,47 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WheaderController {
 
-	@GetMapping({"/","index"})
-	public String MainWheader(Model model) {
+	@GetMapping("/")
+	public String index(Model model) {
 		
-		return "vews/index";
-	}
-	
-	@GetMapping("/layout-static")
-	public String layout_static(Model model) {
-		
-		return "vews/layout-static";
-	}
-	@GetMapping("/layout-sidenav-light")
-	public String layout_sidenav_light(Model model) {
-		
-		return "vews/layout-sidenav-light";
-	}
-	@GetMapping("/login")
-	public String login(Model model) {
-		
-		return "vews/login";
-	}
-	@GetMapping("/register")
-	public String register(Model model) {
-		
-		return "vews/register";
-	}
-	@GetMapping("/password")
-	public String password(Model model) {
-		
-		return "vews/password";
-	}
-	
-	@GetMapping("/charts")
-	public String charts(Model model) {
-		
-		return "vews/charts";
-	}
-	
-	@GetMapping("/tables")
-	public String tables(Model model) {
-		
-		return "vews/tables";
+		return "views/index";
 	}
 }
